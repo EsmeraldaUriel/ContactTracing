725a522e2581b286272c4cc397ee0dbe2030170b
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-11-12 18:10:37 +0800

    "Ask user to select in the menu"

--- ContactTracing.py
+++ ContactTracing.py
@@ -3,3 +3,6 @@ print("==========MENU==========")
 print(" 1 -> Add on item")
 print(" 2 -> Search")
 print(" 2 -> Exit (y/n)")
+
+#Allow user to select item in the menu (check if valid)
+option = int(input("Choose in the menu 1-3: "))
